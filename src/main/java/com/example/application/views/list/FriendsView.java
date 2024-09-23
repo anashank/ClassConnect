c20140c6e93b6b831672fa14e672eb8ce77c3645
ichoe415 <63171186+ichoe415@users.noreply.github.com>
2024-09-22 18:47:59 -0700

    Update FriendsView.java

--- src/main/java/com/example/application/views/list/FriendsView.java
+++ src/main/java/com/example/application/views/list/FriendsView.java
@@ -10,7 +10,6 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
